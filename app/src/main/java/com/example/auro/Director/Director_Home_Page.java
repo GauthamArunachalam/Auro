2d0d51d6ba55ff6dacb1354dc547bc05bdb12a44
AshokAski <ashokmurugavel007@gmail.com>
2019-03-15 22:05:05 +0530

    Registration

--- app/src/main/java/com/example/auro/Director/Director_Home_Page.java
+++ app/src/main/java/com/example/auro/Director/Director_Home_Page.java
@@ -1,5 +1,6 @@
 package com.example.auro.Director;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,14 +8,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.auro.R;
 
 public class Director_Home_Page extends Fragment {
+    private Button registration;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.director_home_page, container, false);
+
+        View view = inflater.inflate(R.layout.director_home_page, container, false);
+
+        registration = view.findViewById(R.id.registration);
+
+        registration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(),Registration.class));
+            }
+        });
+        
+        return view;
     }
 }
